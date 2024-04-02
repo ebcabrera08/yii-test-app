0e9fa5622d11efa1ee1fa33863acf2c8e9704f6e
Edgar Cabrera <ebcabrera08@gmail.com>
2024-04-02 05:31:52 -0300

    Creation of uploads folders

--- models/UploadsModal.php
+++ models/UploadsModal.php
@@ -16,8 +16,12 @@ class UploadsModal extends \yii\db\ActiveRecord
     public $imageFile;
 
     public function upload($path) {       
-        $path = $path . $this->id . "." .pathinfo($this->imageFile->name, PATHINFO_EXTENSION);
-        $this->imageFile->saveAs($path);            
+        $pathImg = $path . $this->id . "." .pathinfo($this->imageFile->name, PATHINFO_EXTENSION);      
+
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);        
+        }
+        $this->imageFile->saveAs($pathImg);            
         $this->picture = $this->id . "." .pathinfo($this->imageFile->name, PATHINFO_EXTENSION);
         $this->imageFile=null;
         return  $this->save();     	
